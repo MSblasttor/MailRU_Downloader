e33a7205aeab4fd58fb283004e03e5251f7b7bc5
Yury Baranov <msblast@bk.ru>
2022-09-16 16:07:16 +0300

    Start dev

--- main.py
+++ main.py
@@ -4,38 +4,45 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from time import sleep
 
 
 def main():
     driver = webdriver.Chrome()
-    driver.get("https://cloud.mail.ru/public/1Swo/65ww21Q3G")
+    #driver.get("https://cloud.mail.ru/public/1Swo/65ww21Q3G")
+    #driver.implicitly_wait(10)
+    driver.get("https://cloud.mail.ru/public/kZkB/WR6UL8w3n")
     #files = driver.find_elements(By.CLASS_NAME,"VirtualList__colItem*")
+    #sleep(10)
+    try:
+        files = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'VirtualList__colItem')]"))
+        )
+    finally:
+        print("We here!")
+        btns_dwl = driver.find_elements(By.XPATH, "//div[contains(@class, 'DataListControl__icon')]")
+    #files = driver.find_elements(By.XPATH, "//div[contains(@class, 'VirtualList__colItem')]")
 
-    while not driver.find_elements(By.XPATH, "//div[contains(@class, 'VirtualList__colItem')]"): sleep(1)
-    files = driver.find_elements(By.XPATH, "//div[contains(@class, 'VirtualList__colItem')]")
-    btns_dwl = driver.find_elements(By.XPATH, "//div[contains(@class, 'DataListControl__icon-')]")
     cnt = 0
     for file in files:
-
         file.click()
-        #print(file.get_property())
+        print(file)
         sleep(1)
         btn_dwl = btns_dwl[cnt]
         cnt += 1
         print(cnt)
-        #btn_dwl = driver.find_element(By.XPATH, "//div[contains(@class, 'DataListControl__icon-')]")
         btn_dwl.click()
-        while not driver.find_element(By.XPATH, "//div[contains(@class, 'Checkbox__root')]"): sleep(1)
-        checkbox = driver.find_element(By.XPATH, "//div[contains(@class, 'Checkbox__root')]")
-        checkbox.click()
-        btn = driver.find_element(By.XPATH, "//button[@data-name='action']")
-        btn.click()
-        sleep(10)
-        #print(btn_dwl)
-
-
+        try:
+            checkbox = driver.find_element(By.XPATH, "//div[contains(@class, 'Checkbox__root')]")
+            checkbox.click()
+            btn = driver.find_element(By.XPATH, "//button[@data-name='action']")
+            btn.click()
+        except NoSuchElementException:
+            print("Not alert window")
 
 
 def print_hi(name):
